d2d8e8428ce6bd4c551d635821c46ad8c2da15e6
akrherz <akrherz@iastate.edu>
2016-12-09 13:18:55 -0600

    iem30 needs certs too

--- jobscript/iemsync.sh
+++ jobscript/iemsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sync LE content to our webfarm nodes
-MACHINES="iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
+MACHINES="iem30 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
 for MACH in $MACHINES
 do
 rsync -a /etc/letsencrypt ${MACH}:/etc/
