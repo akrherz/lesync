d421145e8a133f9d1b02fb6d9526ae12e28fa815
akrherz <akrherz@iastate.edu>
2019-02-22 11:02:46 -0600

    run temp web server for longer period

--- jobscript/iem.sh
+++ jobscript/iem.sh
@@ -2,8 +2,9 @@
 # The workflow the IEM uses, I am run as root
 
 cd /opt/lesync/webroot
-# hack to run this web server just for the next 3 minutes
-timeout 180 python -m SimpleHTTPServer 8081 &
+# hack to run this web server just for the next hour
+# certbot below now has a random sleep
+timeout 3600 python -m SimpleHTTPServer 8081 &
 
 cd /opt/certbot
 # if something got renewed, sync content
