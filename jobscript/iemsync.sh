518ad7679fb13024d33599bab3f59338e808d459
akrherz <akrherz@iastate.edu>
2019-11-15 11:20:31 -0600

    updates

--- jobscript/iemsync.sh
+++ jobscript/iemsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sync LE content to our webfarm nodes
-MACHINES="iem15.local iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
+MACHINES="iem13.local iem15.local iem16.local iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
 for MACH in $MACHINES
 do
 rsync -a /etc/letsencrypt ${MACH}:/etc/
