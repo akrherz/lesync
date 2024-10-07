b277ef86bb9114b3d6a37dac13b22d7daa1b559a
akrherz <akrherz@iastate.edu>
2024-10-07 12:48:29 -0500

    update port

--- jobscript/anticyclone.sh
+++ jobscript/anticyclone.sh
@@ -3,6 +3,7 @@
 cd /opt/lesync/webroot
 # hack to run this web server just for the next hour
 # certbot below now has a random sleep
-timeout 3600 python3 -m http.server 8081 &
+# tomcat has 8081
+timeout 3600 python3 -m http.server 8082 &
 
 certbot renew
